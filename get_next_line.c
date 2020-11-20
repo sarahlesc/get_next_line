68066c491af83b4f528aabf417ed9ea36eb11074
Sarah <sa.lescure@gmail.com>
2020-11-20 16:26:57 +0100

    version 2

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,33 @@
 /*   By: slescure <slescure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 16:18:18 by slescure          #+#    #+#             */
-/*   Updated: 2020/10/16 16:53:55 by slescure         ###   ########.fr       */
+/*   Updated: 2020/11/20 15:46:43 by sarahlescure     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include "get_next_line.h"
+
+int	get_next_line(int fd, char **line)
 {
-	if (argc < 2)
-		return (-1); // l'utilisateur n'a pas rentré de fichier à lire
+	char		buf[BUFFER_SIZE + 1];
+	static char	*save = "";
+	int			r;
 
-
+	if (fd < 0 || BUFFER_SIZE <= 0 || line == NULL)
+		return (-1);
+	while ((ft_strchr(save, '\n') == NULL)
+		&& ((r = read(fd, buf, BUFFER_SIZE)) > 0))
+		{
+			buf[r] = '\0';
+			save = ft_strjoin(save, buf);
+		}
+	if ((!(*line = malloc(sizeof(char) * ft_strlen(save) + 1))) || (r == -1))
+		return (-1);
+	if (r == -1)
+		return (-1);
+	*line = get_line(save);
+	save = get_rest(save);
+	if (r > 0)
+		return (1);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,30 @@
 /*   By: slescure <slescure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 15:23:17 by slescure          #+#    #+#             */
-/*   Updated: 2020/10/16 16:47:20 by slescure         ###   ########.fr       */
+/*   Updated: 2020/11/16 16:18:43 by slescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-int get_next_line(int fd, char **line);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
 
-int count(char **str);
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <fcntl.h>
+
+int 		get_next_line(int fd, char **line);
+size_t		ft_strlen(char *s);
+char		*ft_strchr(char *str, int c);
+char		*ft_strcpy(char *dest, char *src);
+char		*ft_strdup(char *src);
+char		*get_rest(char *str);
+char		*ft_strjoin(char *s1, char *s2);
+char		*get_line(char *str);
+#endif
